bfdb0142467e8a9b6aec08ff3a1179047d633c2b
Jeff Adair <jeffadair@senzing.com>
2021-11-08 11:57:30 -0600

    Updating file names

--- .github/workflows/copy-to-s3.sh
+++ .github/workflows/copy-to-s3.sh
@@ -14,4 +14,4 @@ rm -rf $local_path
 pip3 install boto3
 
 # Run upload script
-python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
+python3 scripts/pipeline/copy-to-s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
