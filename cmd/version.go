3844ecda3aa32673c365c75b7fe9592ffc0c773b
docktermj <docktermj@users.noreply.github.com>
2023-03-30 15:11:58 +0000

    Create cmd/version.go for versioned release: 0.0.73

---
+++ cmd/version.go
@@ -0,0 +1,12 @@
+// Created by make-go-version-file.yaml on Thu Mar 30 15:11:58 UTC 2023
+package cmd
+
+var githubDate            string = "2023-03-30"
+var githubIteration       string = "0"
+var githubRef             string = "refs/tags/0.0.73"
+var githubRefName         string = "0.0.73"
+var githubRepository      string = "Senzing/test-ground"
+var githubRepositoryName  string = "test-ground"
+var githubSha             string = "220d5f07b3b650cbb949b6b6df683302531563a3"
+var githubVersion         string = "0.0.73"
+
