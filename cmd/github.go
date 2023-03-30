382033f6b66e4a4ddc28b12e77bb46f01efb13f7
docktermj <docktermj@users.noreply.github.com>
2023-03-30 21:28:38 +0000

    make-go-version-file.yaml updated cmd/github.go for versioned release: 0.0.6

---
+++ cmd/github.go
@@ -0,0 +1,13 @@
+// 0.0.6
+// Created by make-go-version-file.yaml on Thu Mar 30 21:28:38 UTC 2023
+package cmd
+
+var githubDate            string = "2023-03-30"
+var githubIteration       string = "0"
+var githubRef             string = "refs/tags/0.0.6"
+var githubRefName         string = "0.0.6"
+var githubRepository      string = "Senzing/test-ground"
+var githubRepositoryName  string = "test-ground"
+var githubSha             string = "23ac3eaeb61a9741fa526cbfb5471b95a455b36c"
+var githubVersion         string = "0.0.6"
+
