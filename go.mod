dc2adfbc6f30f2d7f636263c3f71f02c2694ce69
docktermj <michael@dockter.com>
2025-10-27 14:22:01 -0400

    #287 Testcase - 1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/senzing-garage/test-ground
 
 go 1.24.4
+
+require github.com/senzing-garage/go-helpers v0.6.13
--- main.go
+++ main.go
@@ -2,8 +2,103 @@
  */
 package main
 
-import "fmt"
+/*
+#include <stdlib.h>
+#include "libSzConfigMgr.h"
+#include "szhelpers/SzLang_helpers.h"
+#cgo linux CFLAGS: -g -I/opt/senzing/er/sdk/c
+#cgo linux LDFLAGS: -L/opt/senzing/er/lib -lSz
+*/
+import "C"
+
+import (
+	"fmt"
+	"runtime"
+	"unsafe"
+
+	"github.com/senzing-garage/go-helpers/settings"
+)
+
+const (
+	noError      = 0
+	logLevel     = 0
+	instanceName = "test"
+)
+
+// ----------------------------------------------------------------------------
+// Main
+// ----------------------------------------------------------------------------
 
 func main() {
-	fmt.Println("Hello")
+
+	// Build SENZING_ENGINE_CONFIGURATION_JSON.
+
+	senzingEngineSettings, err := settings.BuildSimpleSettingsUsingEnvVars()
+	panicOnError(err)
+	fmt.Printf("SENZING_ENGINE_CONFIGURATION_JSON: %s\n", senzingEngineSettings)
+
+	// Create Configuration manager.
+
+	err = createConfigManager(instanceName, senzingEngineSettings, logLevel)
+	panicOnError(err)
+
+	// Destroy Configuration manager.
+
+	err = destroyConfigManager()
+	panicOnError(err)
+
+	// Create Configuration manager.
+
+	err = createConfigManager(instanceName, senzingEngineSettings, logLevel)
+	panicOnError(err)
+
+	// Destroy Configuration manager.
+
+	err = destroyConfigManager()
+	panicOnError(err)
+
+}
+
+// ----------------------------------------------------------------------------
+// Supporting functions
+// ----------------------------------------------------------------------------
+
+func createConfigManager(
+	instanceName string,
+	settings string,
+	verboseLogging int64,
+) error {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	var err error
+
+	moduleNameForC := C.CString(instanceName)
+	defer C.free(unsafe.Pointer(moduleNameForC))
+
+	iniParamsForC := C.CString(settings)
+	defer C.free(unsafe.Pointer(iniParamsForC))
+
+	result := C.SzConfigMgr_init(moduleNameForC, iniParamsForC, C.int64_t(verboseLogging))
+	if result != noError {
+		err = fmt.Errorf("SzConfigMgr_init failed. Settings: %s", settings)
+	}
+
+	return err
+}
+
+func destroyConfigManager() error {
+	var err error
+
+	result := C.SzConfigMgr_destroy()
+	if result != noError {
+		err = fmt.Errorf("SzConfigMgr_destroy failed")
+	}
+	return err
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
